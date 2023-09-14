614c8507c15b17161d341fe5167d9ff251540667
dabdevs <dabdevs@gmail.com>
2023-09-13 21:07:51 -0300

    layouts

--- src/router.jsx
+++ src/router.jsx
@@ -3,24 +3,39 @@ import App from './App.jsx'
 import Login from './views/Login.jsx'
 import Register from "./views/Register.jsx";
 import Dashboard from "./views/Dashboard.jsx";
+import LandingPage from "./views/LandingPage.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
+import DashboardLayout from "./components/DashboardLayout.jsx";
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <App /> 
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/register',
-        element: <Register />
-    },
-    {
         path: '/dashboard',
-        element: <Dashboard />
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: '',
+                element: <Dashboard />
+            },
+        ]
     },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/',
+                element: <LandingPage />
+            },
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'register',
+                element: <Register />
+            }
+        ]
+    }
 ]);
 
 export default router;
